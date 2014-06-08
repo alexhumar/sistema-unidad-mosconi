652bba134da7dfe9bd8812b8675a043e2719faeb
Alex Humar <alexhumar25@gmail.com>
2014-06-08 19:17:17 -0300

    Agregado el service dialogs_manager

---
+++ src/Salita/OtrosBundle/Service/DialogsManager.php
@@ -0,0 +1,8 @@
+<?php
+namespace Salita\OtrosBundle\Service;
+
+/*Esta clase contiene todos los mensajes del sistema. Deberia ser util a la hora de aplicarle i18n al sistema*/
+class DialogsManager
+{
+	/*Implementar los getters correspondientes a cada mensaje del sistema (auch, que laburo)*/
+}
