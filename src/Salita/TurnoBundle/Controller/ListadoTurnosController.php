6da40f200ad7195ff0b43de5be78b40488b32672
Alex Humar <alexhumar25@gmail.com>
2014-06-15 17:14:10 -0300

    p

--- src/Salita/TurnoBundle/Controller/ListadoTurnosController.php
+++ src/Salita/TurnoBundle/Controller/ListadoTurnosController.php
@@ -23,9 +23,9 @@ class ListadoTurnosController extends Controller
     public function listarEspecialidadAction(Request $request)
     {
         $session = $request->getSession();
-        $usuario = $session->get('usuario');
+        //$usuario = $session->get('usuario');
+        $usuario = $this->get('persistence_manager')->getRepoUserFromSessionUser($session->get('usuario'), $this);
         echo($usuario->getEspecialidad()->getId());die;
-        //$usuario = $this->get('persistence_manager')->getRepoUserFromSessionUser($session->get('usuario'), $this);
         $repoTurnos = $this->get('repos_manager')->getTurnosRepo();
         $rolSeleccionado = ConsultaRol::rolSeleccionado($session);
         $turnos = $repoTurnos->turnosDelDiaDeEspecialidad($usuario->getEspecialidad());
