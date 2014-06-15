c512de66f114ab8bd866c46e0fb04d0d0ba72e60
Alex Humar <alexhumar25@gmail.com>
2014-06-15 16:53:45 -0300

    Pruebas

--- src/Salita/TurnoBundle/Controller/ListadoTurnosController.php
+++ src/Salita/TurnoBundle/Controller/ListadoTurnosController.php
@@ -23,12 +23,14 @@ class ListadoTurnosController extends Controller
     public function listarEspecialidadAction(Request $request)
     {
         $session = $request->getSession();
-        $usuario = $session->get('usuario');
+        echo("Hola 1");
+        $especialidad = $session->get('usuario')->getEspecialidad();
+        echo("Hola 2");
         $repoTurnos = $this->get('repos_manager')->getTurnosRepo();
         $rolSeleccionado = ConsultaRol::rolSeleccionado($session);
         /*Ojo que podria fallar. No estoy seguro que sea un usuario vigilado por el entity manager, asi que
          * podria ser que se sepa traer de la base la especialidad.*/
-        $turnos = $repoTurnos->turnosDelDiaDeEspecialidad($usuario->getEspecialidad());
+        $turnos = $repoTurnos->turnosDelDiaDeEspecialidad($especialidad);
         return $this->render(
         			'SalitaTurnoBundle:Listados:turnosDelDiaEspecialidad.html.twig',
         			array('turnos' => $turnos, 'rol' => $rolSeleccionado->getCodigo())
