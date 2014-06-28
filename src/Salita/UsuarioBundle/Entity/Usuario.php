d8636d308f2b90d0ceb116a46113232d67d729b5
Alex Humar <alexhumar25@gmail.com>
2014-06-28 19:14:36 -0300

    P

--- src/Salita/UsuarioBundle/Entity/Usuario.php
+++ src/Salita/UsuarioBundle/Entity/Usuario.php
@@ -292,7 +292,7 @@ class Usuario extends BaseUser
     
     public function isAuthenticatedAdministrador($rolSeleccionado)
     {
-    	echo(rolSeleccionado);die;
+    	echo($rolSeleccionado);die;
     	return $rolSeleccionado == Rol::getCodigoRolAdministrador();
     }
     
