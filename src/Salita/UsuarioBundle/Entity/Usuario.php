12de907fed8b2cc927c1d4240a04907db6f3296d
Alex Humar <alexhumar25@gmail.com>
2014-06-29 13:01:31 -0300

    P

--- src/Salita/UsuarioBundle/Entity/Usuario.php
+++ src/Salita/UsuarioBundle/Entity/Usuario.php
@@ -288,7 +288,7 @@ class Usuario extends BaseUser
     
     public function isObstetra()
     {
-    	echo($this->getEspecialidad()->getCodigo())die;
+    	echo($this->getEspecialidad()->getCodigo());die;
     	return $this->getEspecialidad()->getCodigo() == Especialidad::getCodigoEspecialidadObstetra();
     }
     
