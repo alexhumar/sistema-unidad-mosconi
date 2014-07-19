19535fa26c80fb6357fef776ffcfdd2b54ab7a38
Alex Humar <alexhumar25@gmail.com>
2014-07-19 10:38:33 -0300

    M

--- src/Salita/PacienteBundle/Form/Type/AntecedentePersonalObstetricoType.php
+++ src/Salita/PacienteBundle/Form/Type/AntecedentePersonalObstetricoType.php
@@ -1,13 +1,14 @@
 <?php
 namespace Salita\PacienteBundle\Form\Type;
 
-use Symfony\Component\Form\AbstractType;
+//use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class AntecedentePersonalObstetricoType extends AbstractType
+class AntecedentePersonalObstetricoType extends AntecedentePersonalType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+    	parent::buildForm($builder, $options);
 
         $builder
             ->add('cirugiaPelviana', 'checkbox', array('label' => 'Cirugía pelviana', 'required' => false))
@@ -21,13 +22,13 @@ class AntecedentePersonalObstetricoType extends AbstractType
             ->add('partos', 'number', array('label' => 'Partos', 'required' => false))
             ->add('sifilis', 'checkbox', array('label' => 'Sífilis', 'required' => false))
             
-            ->add('tuberculosis', 'checkbox', array('label' => 'Tuberculosis', 'required' => false))
+      /*      ->add('tuberculosis', 'checkbox', array('label' => 'Tuberculosis', 'required' => false))
             ->add('diabetes', 'checkbox', array('label' => 'Diabetes', 'required' => false))
             ->add('hipertensionArterial', 'checkbox', array('label' => 'Hipertensión arterial', 'required' => false))
             ->add('drogas', 'checkbox', array('label' => 'Drogas', 'required' => false))
             ->add('tabaquismo', 'checkbox', array('label' => 'Tabaquismo', 'required' => false))
             ->add('alcoholismo', 'checkbox', array('label' => 'Alcoholismo', 'required' => false))
-            ->add('otros', 'textarea', array('label' => 'Otros', 'required' => false))
+            ->add('otros', 'textarea', array('label' => 'Otros', 'required' => false))*/
             
             ->add('modificar', 'submit', array('label' => 'Modificar antecedentes personales obstetricos'));
     }
