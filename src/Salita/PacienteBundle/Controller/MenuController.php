40fd8cad9870201623ce233987f1a825cbb9ef7d
Alex Humar <alexhumar25@gmail.com>
2014-07-20 11:24:40 -0300

    M

--- src/Salita/PacienteBundle/Controller/MenuController.php
+++ src/Salita/PacienteBundle/Controller/MenuController.php
@@ -11,6 +11,11 @@ class MenuController extends MyController
     public function principalAction()
     {
        $session = $this->getSession();
+       /* Si a la hora de registrar "otras anotaciones" (al registrar una consulta) voy al menu 
+        * principal por ejemplo, cuando vuelvo al proceso de registro de consulta me lleva directamente
+        * a otras anotaciones (por como esta hecho el controller), ya sea que el paciente seleccionado sea
+        * el mismo con el que seleccione el diagnostico u otro. No queda bien. */
+       $session->remove('diagnosticoSeleccionado');
        if(!$session->has('paciente'))
        {
            return $this->redirect($this->generateUrl('busqueda_paciente'));
