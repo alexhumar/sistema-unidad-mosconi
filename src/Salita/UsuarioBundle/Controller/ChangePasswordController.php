66ea6394785ac558564d0396ad24bcdb0a95ff57
Alex Humar <alexhumar25@gmail.com>
2014-07-20 12:49:29 -0300

    P

--- src/Salita/UsuarioBundle/Controller/ChangePasswordController.php
+++ src/Salita/UsuarioBundle/Controller/ChangePasswordController.php
@@ -17,17 +17,17 @@ class ChangePasswordController extends BaseController
         if (!is_object($user) || !$user instanceof UserInterface) {
             throw new AccessDeniedException('This user does not have access to this section.');
         }
-
+        echo("hola2");
         $form = $this->container->get('fos_user.change_password.form');
         $formHandler = $this->container->get('fos_user.change_password.form.handler');
-
+        echo("hola3");
         $process = $formHandler->process($user);
         if ($process) {
             $this->container->get('session')->set('mensaje', 'El cambio de contraseña ha sido realizado con éxito');
             $action = 'resultado_operacion_usuario';
             return new RedirectResponse($this->getRedirectionUrl($action));
         }
-
+        echo("hola4");
         return $this->container->get('templating')->renderResponse(
             'FOSUserBundle:ChangePassword:changePassword.html.'.$this->container->getParameter('fos_user.template.engine'),
             array('form' => $form->createView(), 'theme' => $this->container->getParameter('fos_user.template.theme'))
