e79efd674d4a056adeabb9c1ea0f3723dda28395
Alex Humar <alexhumar25@gmail.com>
2014-07-25 20:28:10 -0300

    P

--- src/Salita/PacienteBundle/Form/Type/DatosFiliatoriosType.php
+++ src/Salita/PacienteBundle/Form/Type/DatosFiliatoriosType.php
@@ -13,7 +13,6 @@ class DatosFiliatoriosType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-    	$factory = $builder->getFormFactory();
     	
         $builder
             ->add('tipoDocumento', null, array('label' => 'Tipo de documento'))
@@ -32,78 +31,6 @@ class DatosFiliatoriosType extends AbstractType
             ->add('numero')
             ->add('calleEntre1', null, array('label' => 'Entre calle'))
             ->add('calleEntre2', null, array('label' => 'y calle'));
-        
-        $builder->add('partido', 'entity', array(
-        		'class' => 'SalitaOtrosBundle:Partido',
-        		'property' => 'nombre',
-        		'label' => 'Partido',
-        		'query_builder' =>  function(EntityRepository $er) {
-        			return $er->createQueryBuilder('partido')
-        			->select('partido')
-        			->orderBy('partido.nombre');
-        		},
-        ));
-        
-        $refreshLocalidad =
-        function ($form, $partido) use ($factory)
-        {
-        	$form->add($factory->createNamed('entity', 'localidad', null, array(
-        			'class' => 'SalitaOtrosBundle:Localidad',
-        			'property' => 'nombre',
-        			'label' => 'Localidad',
-        			'query_builder' =>
-        			function (EntityRepository $repository) use ($partido)
-        			{   /* Es el repositorio de la entidad Localidad */
-        				echo ("Hola");
-        				$qb = $repository->localidadesDePartidoQueryBuilder($partido);
-        				return $qb;
-        			}
-        	)));
-        };
-        
-        
-        $refreshBarrio =
-        function($form, $localidad) use ($factory)
-        {
-        	$form->add($factory->createNamed('entity', 'barrio', null, array(
-        			'class' => 'SalitaOtrosBundle:Barrio',
-        			'property' => 'nombre',
-        			'label' => 'Barrio',
-        			'query_builder' =>
-        			function (EntityRepository $repository) use ($localidad)
-        			{
-        				$qb = $repository->barriosDeLocalidadQueryBuilder($localidad);
-        				return $qb;
-        			}
-        	)));
-        };
-
-        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (DataEvent $event) use ($refreshLocalidad, $refreshBarrio) {
-        	$form = $event->getForm();
-        	$data = $event->getData();
-        
-        	if ($data == null){
-        		$refreshLocalidad($form, null);
-        		$refreshBarrio($form, null);
-        	}
-        
-        	if ($data instanceof Paciente) {
-        		$refreshLocalidad($form, $data->getPartido());
-        		$refreshBarrio($form, $data->getLocalidad());
-        	}
-        });
-        
-        	$builder->addEventListener(FormEvents::PRE_BIND, function (DataEvent $event) use ($refreshLocalidad, $refreshBarrio) {
-        		$form = $event->getForm();
-        		$data = $event->getData();
-        
-        		if (array_key_exists('partido', $data)) {
-        			$refreshLocalidad($form, $data['partido']);
-        		}
-        		if (array_key_exists('localidad', $data)) {
-        			$refreshBarrio($form, $data['localidad']);
-        		}
-        	});
     }
     
     public function getDefaultOptions(array $options)
