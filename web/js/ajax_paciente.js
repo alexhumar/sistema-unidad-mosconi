63127220367a454c6e15c0ce23410472c8b60b06
Alex Humar <alexhumar25@gmail.com>
2014-07-25 22:05:53 -0300

    P

--- web/js/ajax_paciente.js
+++ web/js/ajax_paciente.js
@@ -2,26 +2,19 @@
  * Carga dinamica de localidades en base al partido seleccionado
  */
 
-$('#registroDatosFiliatorios_partido').change(
-		function()
-		{
-			$.ajax({
-			    type: "GET",
-				data: "data=" + $(this).val(),
-				url:"{{ path('localidades_de_partido') }}",
-				success:
-					function(msg)
-					{
-					    if(msg != '')
-					    {
-					    	$(#registroDatosFiliatorios_localidad).html(msg).show();
-					    }
-					    else
-					    {
-					    	$(#registroDatosFiliatorios_localidad).html('<em>Sin resultados</em>');
-					    }
-					}
-			});
-         }		
-);
+$('#registroDatosFiliatorios_partido').change(function(){
+    $.ajax({
+       type: "GET",
+	   data: "data=" + $(this).val(),
+       url:"{{ path('localidades_de_partido') }}",
+       success: function(msg){
+		  if(msg != '') {
+		      $(#registroDatosFiliatorios_localidad).html(msg).show();
+		      }
+		      else {
+	   	          $(#registroDatosFiliatorios_localidad).html('<em>Sin resultados</em>');
+		      }
+	      }
+		});
+    });
 
