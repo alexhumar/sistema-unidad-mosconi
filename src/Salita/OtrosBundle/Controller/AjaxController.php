753b2d570fdf2bc6db92710b377e8244580a55c1
Alex Humar <alexhumar25@gmail.com>
2014-07-25 21:54:45 -0300

    P

--- src/Salita/OtrosBundle/Controller/AjaxController.php
+++ src/Salita/OtrosBundle/Controller/AjaxController.php
@@ -8,6 +8,8 @@ use Salita\OtrosBundle\Clases\BusquedaDiagnostico;
 use Salita\OtrosBundle\Clases\MyController;
 use Salita\OtrosBundle\Clases\ConsultaRol;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class AjaxController extends MyController
 {
 
@@ -17,9 +19,15 @@ class AjaxController extends MyController
     	$repoPartidos = $this->getReposManager()->getPartidosRepo();
     	$idPartido = $this->getRequest()->query->get('data');
     	$localidades = $repoPartidos->localidadesDePartido($idPartido);
-    	return $this->render(
+    	$html = '';
+    	foreach ($localidades as $localidad)
+    	{
+    		$html = $html . sprintf("<option value=\"%d\">%s</option>", $localidad->getId(), $localidad->getNombre());
+    	}
+    	/*return $this->render(
 	           		'SalitaOtrosBundle:Ajax:localidadesDePartido.html.twig',
     			    array('localidades' => $localidades)
-    			);
+    			);*/
+    	return new Response($html);
     }
 }
