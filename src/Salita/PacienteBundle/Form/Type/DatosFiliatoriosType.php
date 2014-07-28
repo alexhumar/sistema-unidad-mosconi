06f96a5881cb20b09cf5166ddb5caaf2b9dbd02a
Alex Humar <alexhumar25@gmail.com>
2014-07-28 19:45:07 -0300

    P

--- src/Salita/PacienteBundle/Form/Type/DatosFiliatoriosType.php
+++ src/Salita/PacienteBundle/Form/Type/DatosFiliatoriosType.php
@@ -70,7 +70,10 @@ class DatosFiliatoriosType extends AbstractType
 		    function (FormInterface $form, Localidad $localidad = null)
 		    {
 		    	$barrios = null === $localidad ? array() : $localidad->getBarrios();
-		    	echo(var_dump($barrios));die;
+		    	if (null !== $localidad)
+		    	{
+		    		echo(var_dump($barrios));
+		    	}
 		    	$form->add('barrio', 'entity', array(
 		    		       'class' => 'SalitaOtrosBundle:Barrio',
 		    			   'empty_value' => '',
