f920cc0ead6221a33840236a6824fa90b440a941
Alex Humar <alexhumar25@gmail.com>
2014-07-28 20:49:06 -0300

    P

--- src/Salita/PacienteBundle/Form/Type/DatosFiliatoriosType.php
+++ src/Salita/PacienteBundle/Form/Type/DatosFiliatoriosType.php
@@ -47,7 +47,7 @@ class DatosFiliatoriosType extends AbstractType
 	    		  'empty_value' => false
 	    ));
     
-	    $setLocalidad =
+	    $refreshLocalidad =
 		    function (FormInterface $form, Partido $partido = null)// use ($factory)
 		    {
 		    	$localidades = null === $partido ? array() : $partido->getLocalidades();	    	
@@ -58,7 +58,7 @@ class DatosFiliatoriosType extends AbstractType
 		    	));
 		    };
 		    
-		$setBarrio = 
+		$refreshBarrio = 
 		    function (FormInterface $form, Localidad $localidad = null)
 		    {
 		    	$barrios = null === $localidad ? array() : $localidad->getBarrios();
@@ -68,46 +68,46 @@ class DatosFiliatoriosType extends AbstractType
 		    			   'choices' => $barrios
 		    	));
 		    };
-		    
-		    $refreshLocalidad =
-		    function (FormInterface $form, Partido $partido = null)// use ($factory)
-		    {
-		    	$localidades = null === $partido ? array() : $partido->getLocalidades();
-		    	$form->get('localidad')->setData($localidades);
-		    };
-		    
-		    $refreshBarrio =
-		    function (FormInterface $form, Localidad $localidad = null)
-		    {
-		    	$barrios = null === $localidad ? array() : $localidad->getBarrios();
-		    	$form->get('barrio')->setData($barrios);
-		    };
-		    
-    
+
 	    $builder
 	        ->addEventListener(
-	        		FormEvents::PRE_SUBMIT,//POST_SET_DATA, 
-	        		function (FormEvent $event) use ($setLocalidad, $setBarrio) {
+	        		FormEvents::PRE_SUBMIT, //POST_SET_DATA, 
+	        		function (FormEvent $event) use ($refreshLocalidad, $refreshBarrio) {
 	    	            $form = $event->getForm();
 	    	            $paciente = $event->getData();
 	    		        $setLocalidad($form, $paciente->getPartido());
 	    		        $setBarrio($form, $paciente->getLocalidad());
 	        });
 	    
-	    $builder
-	        ->addEventListener(
-	    		FormEvents::POST_SUBMIT,//POST_SET_DATA,
-	    		function (FormEvent $event) use ($refreshLocalidad, $refreshBarrio) {
+	    $builder->get('partido')->addEventListener(
+	    		FormEvents::POST_SUBMIT,
+	    		function (FormEvent $event) use ($refreshLocalidad) {
 	    			$form = $event->getForm();
-	    			$paciente = $event->getData();
-	    			echo(var_dump($paciente->getPartido()));
-	    			echo(var_dump($paciente->getLocalidad()));
-	    			die;
-	    			$refreshLocalidad($form, $paciente->getPartido());
-	    			$refreshBarrio($form, $paciente->getLocalidad());
-	    		});
 	    
-
+	    			/* Es importante capturarlo de esta manera ya que $event->getData() retorna la client data
+	    			 * (o sea, el ID). Esto estaba en el cookbook. Lo anoto para que quede. */
+	    			$partido = $event->getForm()->getData();
+	    				
+	    			/* Como el listener se agrego al hijo, tenemos que pasarlo el form padre a las funciones
+	    			 * callback (estaba en el cookbook), no me cierra del todo */
+	    			$refreshLocalidad($form->getParent(), $partido);
+	    		});
+	     
+	    //ATENCION: no me esta agregando esto como event listener... verificar.
+	    $builder->get('localidad')->addEventListener(
+	    		FormEvents::POST_SUBMIT,
+	    		function (FormEvent $event) use ($refreshBarrio) {
+	    			$form = $event->getForm();
+	    			 
+	    			/* Es importante capturarlo de esta manera ya que $event->getData() retorna la client data
+	    			 * (o sea, el ID). Esto estaba en el cookbook. Lo anoto para que quede. */
+	    			$localidad = $event->getForm()->getData();
+	                echo(var_dump($localidad));die;
+	    			 
+	    			/* Como el listener se agrego al hijo, tenemos que pasarlo el form padre a las funciones
+	    			 * callback (estaba en el cookbook), no me cierra del todo */
+	    			$refreshBarrio($form->getParent(), $localidad);
+	    		});
     }
     
     public function getDefaultOptions(array $options)
