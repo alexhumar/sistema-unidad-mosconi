b7b450c9fe9e82bdb80b83b9077bb8e92068ff42
alexhumar <alexhumar25@gmail.com>
2014-11-10 20:36:22 -0300

    Agregada funcionalidad de carga de observaciones para el resumen de historia clinica

--- src/Salita/PacienteBundle/Controller/PacienteFormController.php
+++ src/Salita/PacienteBundle/Controller/PacienteFormController.php
@@ -3,6 +3,7 @@ namespace Salita\PacienteBundle\Controller;
 
 use Salita\PacienteBundle\Form\Type\DatosFiliatoriosModificacionType;
 use Salita\PacienteBundle\Form\Type\DatosFiliatoriosRegistroType;
+use Salita\PacienteBundle\Form\Type\ObservacionesHCType;
 use Salita\PacienteBundle\Entity\Paciente;
 use Salita\OtrosBundle\Clases\MyController;
 
@@ -56,5 +57,32 @@ class PacienteFormController extends MyController
            			'SalitaPacienteBundle:PacienteForm:new.html.twig', 
            			array('form' => $form->createView())
            		);
+    }
+    
+    /*Modificacion de observaciones para el resumen de historia clinica*/
+    public function modificarObservacionesHCAction()
+    {
+    	$session = $this->getSession();
+    	$paciente = $session->get('paciente');
+    	$repoPacientes = $this->getReposManager()->getPacientesRepo();
+    	$paciente = $repoPacientes->find($paciente->getId());
+    	$form = $this->createForm(new ObservacionesHCType(), $paciente);
+    	$request = $this->getRequest();
+    	$form->handleRequest($request);
+    	if ($form->isValid())
+    	{
+    		$em = $this->getEntityManager();
+    		$em->flush();
+    		/* Actualiza el paciente guardado en la sesion */
+    		$session->set('paciente', $paciente);
+    		$translator = $this->getTranslator();
+    		$mensaje = $this->getDialogsManager()->modificacionDatosPacienteExitoMsg();
+    		$session->set('mensaje', $translator->trans($mensaje));
+    		return $this->redirect($this->generateUrl('resultado_operacion_paciente'));
+    	}
+    	return $this->render(
+    			'SalitaPacienteBundle:PacienteForm:observacionesHC.html.twig',
+    			array('form' => $form->createView())
+    	);
     }
 }
